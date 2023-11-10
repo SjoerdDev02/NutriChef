b5186f5808ed939fa6c1071d557a8cc776a31eb7
Sjoerd Kessels <sjoerd.kessels2002@gmail.com>
2023-11-10 14:55:51 +0100

    protected route added

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,36 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate, Outlet } from 'react-router-dom';
 import Home from './assets/pages/Home/Home';
 import Recipes from './assets/pages/Recipes/Recipes';
 import Checkout from './assets/pages/Checkout/Checkout';
 import NotFound from './assets/pages/NotFound/NotFound';
+import { useSelector } from 'react-redux';
+import { CartItemType, CartType } from './assets/types/CartType';
+
+const ProtectedRoute = ({ cart }: { cart: CartItemType[] }) => {
+    if (cart.length === 0) {
+        return <Navigate to='/' replace />;
+    }
+
+    return <Outlet />;
+};
 
 function App() {
-  return (
-      <>
-          <BrowserRouter>
-              <Routes>
-                  <Route path='/' element={<Home />} />
-                  <Route path='/recipes' element={<Recipes />} />
-                  <Route path='checkout' element={<Checkout />} />
-                  <Route path='*' element={<NotFound />} />
-              </Routes>
-          </BrowserRouter>
-      </>
-  );
+    const cart = useSelector((state: CartType) => state.cart.cart);
+
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                    <Route path='/recipes' element={<Recipes />} />
+                    <Route element={<ProtectedRoute cart={cart} />}>
+                        <Route path='checkout' element={<Checkout />} />
+                    </Route>
+                    <Route path='*' element={<NotFound />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
