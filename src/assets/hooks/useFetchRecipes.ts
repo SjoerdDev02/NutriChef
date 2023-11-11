633f4fbe455b2b9b10a117eb33bc92eb8a0f9da7
SjoerdDev02 <137709448+SjoerdDev02@users.noreply.github.com>
2023-11-11 15:56:30 +0100

    Update useFetchRecipes.ts

--- src/assets/hooks/useFetchRecipes.ts
+++ src/assets/hooks/useFetchRecipes.ts
@@ -5,8 +5,8 @@ const useFetchRecipes = () => {
     const cuisine = kitchen ? `&cuisineType=${kitchen}` : '';
     const diets = diet ? `&diet=${diet}` : '';
 
-    const APP_ID = 'f2759b01';
-    const APP_KEY = '249295e2e1d587c5b1abf2b3007b7162';
+    const APP_ID = 'YOUR_ID';
+    const APP_KEY = 'YOUR_KEY';
 
     const url = `https://api.edamam.com/api/recipes/v2?type=public&beta=false&app_id=${APP_ID}&app_key=${APP_KEY}${diets}&health=vegan${cuisine}&measure=gram&field=label&field=images&field=dietLabels&field=ingredientLines&field=totalWeight&field=totalTime&field=cuisineType&field=totalNutrients&field=totalTime`;
 
